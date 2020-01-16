f7f0b6fe6c208eb53bbfe37782979623978f2669
ikeryou <ikeda.ryou@gmail.com>
2020-01-16 17:10:10 +0900

    コード、説明しやすく

--- 02-3D-Programming/01/script.js
+++ 02-3D-Programming/01/script.js
@@ -1,6 +1,6 @@
 let _renderer, _mainScene, _mainCamera, _screenWidth, _screenHeight
 
-let _light, _material
+let _light
 let _mesh = []
 let _now = 0
 let _cnt = 0
@@ -9,27 +9,38 @@ let _noise = 0
 // initialization
 function init() {
 
-  // make material
-  // can be reused
-  _material = new THREE.MeshPhongMaterial({
+  // ####################################
+  // ####################################
+
+  // box mesh
+  const geoA = new THREE.BoxBufferGeometry(1, 1, 1, 8, 8, 8)
+  const matA = new THREE.MeshPhongMaterial({
     color: 0xffffff, // color when light hits
     side: THREE.DoubleSide,
   })
-
-  // box geometry and mesh
-  const geoA = new THREE.BoxBufferGeometry(1, 1, 1, 8, 8, 8)
-  const meshA = new THREE.Mesh(geoA, _material)
+  const meshA = new THREE.Mesh(geoA, matA)
   _mesh.push(meshA)
 
-  // Torus gerometry and mesh
+  // Torus mesh
   const geoB = new THREE.TorusBufferGeometry(0.75, 0.2, 8, 20)
-  const meshB = new THREE.Mesh(geoB, _material)
+  const matB = new THREE.MeshPhongMaterial({
+    color: 0xffffff, // color when light hits
+    side: THREE.DoubleSide,
+  })
+  const meshB = new THREE.Mesh(geoB, matB)
   _mesh.push(meshB)
 
-  // octahedron geometry and mesh
+  // octahedron mesh
   const geoC = new THREE.OctahedronBufferGeometry(0.75, 0)
-  const meshC = new THREE.Mesh(geoC, _material)
+  const matC = new THREE.MeshPhongMaterial({
+    color: 0xffffff, // color when light hits
+    side: THREE.DoubleSide,
+  })
+  const meshC = new THREE.Mesh(geoC, matC)
   _mesh.push(meshC)
+
+  // ####################################
+  // ####################################
 
   // make main scene
   _mainScene = new THREE.Scene()
@@ -39,6 +50,9 @@ function init() {
   _mainScene.add(meshB)
   _mainScene.add(meshC)
 
+  // ####################################
+  // ####################################
+
   // make light
   _light = new THREE.PointLight(
     0xffffff, // light color
@@ -46,8 +60,14 @@ function init() {
   )
   _mainScene.add(_light)
 
+  // ####################################
+  // ####################################
+
   // make main camera
   _mainCamera = new THREE.PerspectiveCamera(80, 1, 0.1, 50000)
+
+  // ####################################
+  // ####################################
 
   // make renderer
   _renderer = new THREE.WebGLRenderer({
@@ -56,6 +76,9 @@ function init() {
   })
   _renderer.setClearColor(0xe1e1e3, 1) // background color
   _renderer.setPixelRatio(window.devicePixelRatio || 1)
+
+  // ####################################
+  // ####################################
 
   change()
 
@@ -67,63 +90,104 @@ function init() {
 
 // update something when window resized
 function resize() {
+
   _screenWidth = window.innerWidth
   _screenHeight = window.innerHeight
+
+  // ####################################
+  // ####################################
 
   // camera settings for to be actual size
   _mainCamera.aspect = _screenWidth / _screenHeight
   _mainCamera.updateProjectionMatrix()
   _mainCamera.position.z = (_screenHeight * 0.5) / Math.tan((_mainCamera.fov * 0.5) * Math.PI / 180)
 
+  // ####################################
+  // ####################################
+
   _renderer.setSize(_screenWidth, _screenHeight)
+
+  // ####################################
+  // ####################################
 
   // update something
   const size = Math.min(_screenWidth, _screenHeight) * 0.25
   _mesh[0].scale.set(size, size, size)
   _mesh[1].scale.set(size, size, size)
   _mesh[2].scale.set(size, size, size)
+
+  // ####################################
+  // ####################################
 }
 
 
 // update every frame
 function update() {
+
+  // ####################################
+  // ####################################
+
   // update some thing for animations
   _noise += (0 - _noise) * 0.6
 
   // when _noise > 0
   // randomize color
   const color = new THREE.Color(0xffffff)
-  _material.color = color.lerp(new THREE.Color(Math.random(), Math.random(), Math.random()), _noise * 30)
+  _mesh[0].material.color = color.lerp(new THREE.Color(Math.random(), Math.random(), Math.random()), _noise * 30)
+  _mesh[1].material.color = color.lerp(new THREE.Color(Math.random(), Math.random(), Math.random()), _noise * 30)
+  _mesh[2].material.color = color.lerp(new THREE.Color(Math.random(), Math.random(), Math.random()), _noise * 30)
 
   if (_noise >= 0.0001) {
-    // alternate between true and false
     if (_cnt % 4 == 0) {
-      _material.wireframe = !_material.wireframe
+      // alternate between true and false
+      _mesh[0].material.wireframe = !_mesh[0].material.wireframe
+      _mesh[1].material.wireframe = !_mesh[1].material.wireframe
+      _mesh[2].material.wireframe = !_mesh[2].material.wireframe
     }
   } else {
-    _material.wireframe = false
+    _mesh[0].material.wireframe = false
+    _mesh[1].material.wireframe = false
+    _mesh[2].material.wireframe = false
   }
+
+  // ####################################
+  // ####################################
 
   // udpate camera position and look at container
   const radian = new Date().getTime() * 0.002 * mix(1, 1.001, _noise)
   const radius = Math.min(_screenWidth, _screenHeight) * 0.5
   _mainCamera.position.x = Math.sin(radian) * radius
   _mainCamera.position.z = Math.cos(radian) * radius
+
+  // ####################################
+  // ####################################
+
   _mainCamera.lookAt(new THREE.Vector3(0, 0, 0))
+
+  // ####################################
+  // ####################################
 
   // update light position
   _light.position.copy(_mainCamera.position)
   _light.position.y += _screenHeight * 0.25
 
+  // ####################################
+  // ####################################
+
   // change 3D object
   if (_cnt % 120 == 0) {
     change()
   }
-
   _cnt++
+
+  // ####################################
+  // ####################################
 
   // rendering
   _renderer.render(_mainScene, _mainCamera)
+
+  // ####################################
+  // ####################################
 
   window.requestAnimationFrame(update)
 }
@@ -131,6 +195,15 @@ function update() {
 
 // change mesh
 function change() {
+
+  // ####################################
+  // ####################################
+
+  // parameter for replacing effect
+  _noise = 1
+
+  // ####################################
+  // ####################################
 
   // show only one
   for (let i = 0, len = _mesh.length; i < len; i++) {
@@ -140,6 +213,9 @@ function change() {
   if (_now >= _mesh.length) {
     _now = 0
   }
+
+  // ####################################
+  // ####################################
 
   // angle randomly
   _mesh[0].rotation.set(
@@ -158,8 +234,8 @@ function change() {
     Math.random() * 360 * (Math.PI / 180)
   )
 
-  // parameter for replacing effect
-  _noise = 1
+  // ####################################
+  // ####################################
 }
 
 function mix(x, y, a) {
