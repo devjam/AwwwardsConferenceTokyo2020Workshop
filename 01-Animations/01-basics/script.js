a1d4d00a6ba650b92093b530678a99ea17a325e9
Hiroaki Yasutomo <yasutomo@shiftbrain.co.jp>
2020-01-21 11:24:41 +0900

    update

--- 01-Animations/01-basics/script.js
+++ 01-Animations/01-basics/script.js
@@ -1,23 +1,28 @@
-var _renderer, _mainScene, _mainCamera, _container, _material
-var _mesh = []
-var _current = 0
-var _randomSeed = (Math.random() * 1000000) | 0
+let _renderer, _mainScene, _mainCamera, _container, _material
 
-var _frame = 1000
-var _fps = 100 //_frame;
-var _size = 100
+const _mesh = []
+const _randomSeed = (Math.random() * 1000000) | 0
+const _size = 100
 
-var _scaleSpeed = 0.05
-var _colorSpeed = 0.005
-var _rotationSpeed = 0.1
+let _current = 0
 
-var _showAll = 0
-var _useRandom = 0
-var _usePosition = 1
-var _useRotation = 1
-var _useScale = 1
-var _useColor = 1
+const _params = {
+  frame: 1000,
+  fps: 12,
+  
+  rotationSpeed: 0.1,
+  scaleSpeed: 0.05,
+  colorSpeed: 0.005,
 
+  showAll: true,
+  useRandom: false,
+  usePosition: true,
+  useRotation: false,
+  useScale: false,
+  useColor: false,
+}
+
+const gui = new dat.GUI()
 
 class Random {
   constructor(seed = 88675123) {
@@ -62,14 +67,35 @@ function init() {
 
   _material = new THREE.MeshBasicMaterial({
     color: new THREE.Color(0x00e1e1).offsetHSL(Math.random(), 0, 0),
+    blending: THREE.NormalBlending,
+    transparent: true,
+    opacity: 0.5,
   })
 
-  var geo = new THREE.BoxBufferGeometry(_size, _size, 0)
-  for (var i = 0; i < _frame; i++) {
+  const geo = new THREE.BoxBufferGeometry(_size, _size, 0)
+  for (var i = 0; i < _params.frame; i++) {
     var mesh = new THREE.Mesh(geo, _material)
     _container.add(mesh)
     _mesh.push(mesh)
   }
+
+  _params.frame = 2
+  // dat.gui
+  gui.add(_params, 'frame', 1, 1000, 1)
+  gui.add(_params, 'fps', 1, 100, 1)
+
+  gui.add(_params, 'showAll')
+  gui.add(_params, 'useRandom')
+  gui.add(_params, 'usePosition')
+
+  gui.add(_params, 'useRotation')
+  gui.add(_params, 'rotationSpeed', 0.01, 0.5, 0.01)
+
+  gui.add(_params, 'useScale')
+  gui.add(_params, 'scaleSpeed', 0.005, 0.5, 0.01)
+
+  gui.add(_params, 'useColor')
+  gui.add(_params, 'colorSpeed', 0.001, 0.01, 0.001)
 
   window.addEventListener('resize', resize)
   resize()
@@ -78,41 +104,50 @@ function init() {
 }
 
 function update() {
-  if (_useColor) _material.color = _material.color.offsetHSL(_colorSpeed, 0, 0)
+  if (_params.useColor) _material.color = _material.color.offsetHSL(_params.colorSpeed, 0, 0)
 
-  var w = window.innerWidth - _size * 2
-  var start = -w * 0.5
-  var step = w / (_frame - 1 ? _frame - 1 : 1)
-  var r = new Random(_randomSeed)
-  for (var i = 0; i < _mesh.length; i++) {
-    var mesh = _mesh[i]
-    if (_usePosition) mesh.position.x = start + i * step
+  const w = window.innerWidth - _size * 2
+  const start = -w * 0.5
+  const step = w / (_params.frame - 1 ? _params.frame - 1 : 1)
+
+  const r = new Random(_randomSeed)
+  for (let i = 0; i < _mesh.length; i++) {
+    const mesh = _mesh[i]
+    if (_params.usePosition) mesh.position.x = start + i * step
     else mesh.position.x = 0
 
-    if (_useRandom)
-      mesh.position.set(
-        (r.nextInt(0, 1000) / 1000) * w + start,
-        (r.nextInt(0, 1000) / 1000) * w + start,
-        0,
-      )
+    if (_params.useRandom) {
+      if(i == 0 || i == _params.frame - 1) {
+        mesh.position.x = _params.usePosition ? start + i * step : 0
+        mesh.position.y = 0
+      } else {
+        mesh.position.x = _params.usePosition ? start + i * step : (r.nextInt(0, 1000) / 1000) * w + start
+        mesh.position.y = (r.nextInt(0, 1000) / 1000) * w + start
+      }
+    } else {
+      mesh.position.y = 0
+    }
 
-    if (_useRotation) mesh.rotation.z = _rotationSpeed * i
+    if (_params.useRotation) mesh.rotation.z = _params.rotationSpeed * i
+    else mesh.rotation.z = 0
 
-    if (_useScale)
-      mesh.scale.set(Math.cos(i * _scaleSpeed) * 0.5 + 1, Math.sin(i * _scaleSpeed) * 0.5 + 1, 1)
+    if (_params.useScale)
+      mesh.scale.set(Math.cos(i * _params.scaleSpeed) * 0.5 + 1, Math.sin(i * _params.scaleSpeed) * 0.5 + 1, 1)
+    else
+      mesh.scale.set(1,1,1) 
 
-    if (_showAll || _current == i) mesh.visible = true
+    if (i < _params.frame && (_params.showAll || _current == i)) mesh.visible = true
     else mesh.visible = false
   }
 
   _current++
-  _current %= _frame
+  _current %= _params.frame
 
   _renderer.render(_mainScene, _mainCamera)
 
   setTimeout(function() {
     requestAnimationFrame(update)
-  }, 1000 / _fps)
+  }, 1000 / _params.fps)
 }
 
 function resize() {
